3c7d61e7826b605f00f46ac08c86f0a788a79c15
TJaiwanth3 <jaiwanthtatuskar2@gmail.com>
2025-06-30 10:13:37 +0530

    Context Api

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Learning Context API</h1>
+      <Login />
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
